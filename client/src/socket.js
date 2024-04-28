01f445144c22be61ec4f5a1d8ff75ba920400f20
sfanning5 <stephen.e.fanning@gmail.com>
2024-04-28 16:49:26 -0400

    Update socket URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client"
 
-const socket = io('localhost:8080') // TODO: this might not let things work across computers
+// const socket = io('localhost:8080')
+const socket = io('https://chess-temp.onrender.com') // TODO: this might not let things work across computers
 
 export default socket
