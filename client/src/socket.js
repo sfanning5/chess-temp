8bb865312dc64eed5cca01b5a9bf07e11395437d
sfanning5 <stephen.e.fanning@gmail.com>
2024-04-29 12:04:28 -0400

    Fixed socket for render

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-// const socket = io('localhost:8080')
-const socket = io('chess-temp.onrender.com', { transports: ['websocket'] }) // TODO: this might not let things work across computers
+// const socket = io('localhost:8080') // TODO: set it to render
+const socket = io('chess-temp.onrender.com', { transports: ['websocket'] }) 
 
 export default socket
